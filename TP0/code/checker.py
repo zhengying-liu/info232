3ee8ae856a2234e4ff2b3e00650772d27a6301c9
madclam@gmail.com <madclam@gmail.com>
2020-01-12 16:29:11 -0800

    TP0

--- TP0/code/checker.py
+++ TP0/code/checker.py
@@ -1,8 +1,8 @@
-truth = [1, 420, 7.3982790000000005, 1, 3, 6, 2.97, 0.10555555555555551, 1.2, 3]
+truth = [1, 420, 37571135275072476912863460334485873127534, 7.3982790000000005, 2056.130, 1, 4, 3, 6, 2.97]
 
 def check(answer, question):
-	#print(answer)
-	#print(truth[question-1])
+	print(answer)
+	print(truth[question-1])
 	if abs(answer - truth[question-1])<0.0001:
 		get_ipython().run_cell_magic(u'HTML', u'', u'<div style="background:#00FF00">CORRECT<br>:-)</div>')
 		return 1
