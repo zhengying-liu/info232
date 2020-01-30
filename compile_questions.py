3f381b6e0eb02d244d74de608f04ee70dac805ae
Victor Estrade <victor.estrade@lri.fr>
2020-01-30 19:12:17 +0100

    minor

--- compile_questions.py
+++ compile_questions.py
@@ -4,7 +4,7 @@ from subprocess import call
 
 all_files = os.listdir('./')
 all_dirs = filter(os.path.isdir, all_files)
-TP = list(filter(lambda fname: fname.startswith("TP"), all_files))
+TP = list(filter(lambda fname: fname.startswith("PT"), all_files))
 
 for tp in TP:
 	cmd = ['python', '-m', 'compileall', f'{tp}/questions.py', '-b']
