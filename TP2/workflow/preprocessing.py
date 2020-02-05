dcc91ac1fb6f3747877362c2cd856aef9e34ab54
victor estrade <victor.estrade@u-psud.fr>
2020-02-05 14:47:25 +0100

    to_histogram converted as sklearn preprocessing

--- TP2/workflow/preprocessing.py
+++ TP2/workflow/preprocessing.py
@@ -1,13 +1,29 @@
 import numpy as np
 
 
-def image_histogram(image):
-    count = np.zeros(256)
-    for pix_value in image:
-        count[pix_value] += 1
-    return count
+class ImageHistogram(object):
+    """
+    La doc !
+    """
+    def __init__(self):
+        pass
+    
+    def fit(self, X, y=None):
+        """
+        Do nothing
+        """
+        return self
+    
+    def transform(self, X):
+        """
+        ToDo Doc
+        """
+        X_new = np.array([self._image_histogram(img) for img in X])
+        return X_new
 
+    def _image_histogram(self, image):
+        count = np.zeros(256)
+        for pix_value in image:
+            count[pix_value] += 1
+        return count
 
-def to_histogram(X):
-    X_new = np.array([image_histogram(img) for img in X])
-    return X_new
