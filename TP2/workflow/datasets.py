f6b5d13e2c0d08df345703ac1b2d9df8b14b5246
victor estrade <victor.estrade@u-psud.fr>
2020-02-05 14:10:50 +0100

    add make_data with sklearn moons

--- TP2/workflow/datasets.py
+++ TP2/workflow/datasets.py
@@ -3,9 +3,12 @@
 
 import os
 from PIL import Image
+from sklearn.datasets import make_moons
+
 
 DATA_DIR = "./DATA/mini-dataset/"
 
+#TODO: fix inconsistent return types for data laoders 
 
 def load_data():
     """
@@ -19,3 +22,8 @@ def load_data():
     all_images = [Image.open(os.path.join(DATA_DIR, fname)) for fname in all_files]
     return all_images
 
+
+
+def make_data():
+    X, y = make_moons(n_samples=500, noise=0.01 )
+    return X, y
